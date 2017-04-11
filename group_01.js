f6d426f310d257642974adaf723e9f06dcea824d
Colin Wymore <colinwymore@gmail.com>
2017-04-11 11:00:04 -0500

    updated

--- group_01.js
+++ group_01.js
@@ -6,3 +6,42 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+// var new array [name, bonus%, slary+bonus, bonusRounded]
+
+var newArray = [];
+
+function calcBonus (name, employeeNumber, salary, rating){
+  var bonus = 0;
+
+  if(rating <= 2){
+      bonus += 0;
+  }else if(rating === 3) {
+      bonus += 0.04;
+  }else if(rating === 4) {
+      bonus += 0.06;
+  }else{
+    bonus += 0.10;
+  }
+  if(employeeNumber.length === 4){
+      bonus += 0.05;
+  } // currently calculating passed 13%. We are not sure why?!?
+  // if(salary >= '65000'){
+  //   bonus -= 0.01;
+  // }
+  // if(bonus > 0.13){
+  //   bonus += 0.13;
+  // } else if(bonus < 0.00){
+  //   bonus += 0.00;
+  // }
+  console.log(salary*bonus);
+// console.log(name, employeeNumber, salary, rating);
+  // return name;
+}
+calcBonus(scout[0], scout[1], scout[2], scout[3]);
+console.log(calcBonus(scout[0], scout[1], scout[2], scout[3]));
+
+
+
+
+console.log ("test");
